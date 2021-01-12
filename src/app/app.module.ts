df6f00484e8d12a1a86478f6bceb881b3c998986
Luca Schmider <luca.schmider@gmail.com>
2021-01-12 22:21:11 +0100

    finished style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { EffectsModule } from '@ngrx/effects';
 import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { NgModule } from '@angular/core';
@@ -16,8 +15,7 @@ import { environment } from '../environments/environment';
   declarations: [
     AppComponent,
     FooterComponent,
-    ToastViewComponent,
-    HeaderComponent
+    ToastViewComponent
   ],
   imports: [
     BrowserModule,
