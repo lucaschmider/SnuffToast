cc491c13024d41a4db023ba20e4a91c51b6e6898
Luca Schmider <luca.schmider@gmail.com>
2021-01-23 21:33:41 +0100

    implement control value accessor

--- src/app/mode-switch/mode-switch.component.ts
+++ src/app/mode-switch/mode-switch.component.ts
@@ -1,16 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
+import { Mode } from "./mode";
 
 @Component({
   selector: 'snuff-mode-switch',
   templateUrl: './mode-switch.component.html',
   styleUrls: ['./mode-switch.component.scss']
 })
-export class ModeSwitchComponent implements OnInit {
-  public active: boolean = false;
+export class ModeSwitchComponent implements ControlValueAccessor {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  public mode: Mode = Mode.All;
+  public disabled: boolean = false;
+  public readonly MODES = Mode;
+  private onChange: () => void;
+  private onTouched: () => void;
+
+  public toggle(): void {
+    this.mode = this.mode === Mode.All ? Mode.FavouritesOnly : Mode.All;
+    if (this.onChange !== undefined) this.onChange();
+    if (this.onTouched !== undefined) this.onTouched();
+  }
+
+  public writeValue(obj: any): void {
+    this.mode = obj === Mode.FavouritesOnly ? Mode.FavouritesOnly : Mode.All;
+  }
+
+  public registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  public setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
   }
 
 }
