638c0d3a7097ef4976b4b5a462f12afa206c70ba
Luca Schmider <luca.schmider@outlook.com>
2021-07-08 12:04:09 +0200

    add capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "ninja.schmider.snufftoast",
+  appName: "SnuffToast",
+  webDir: "dist/SnuffToast",
+  bundledWebRuntime: false
+};
+
+export default config;
